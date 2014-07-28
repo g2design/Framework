fdf99b564ecbff77c49f29de7d45357596689b68
Stephan Wessels <stephan@g2design.co.za>
2014-07-28 16:35:39 +0200

    Added Git add -A to script

--- commit.php
+++ commit.php
@@ -36,9 +36,12 @@ if(!empty($_POST) && $_POST['password'] == $password) {
 	} else {
 		$message = addslashes($message);
 		$commands = [
+			' git config --global user.name "Stephan"',
+			'git config --global user.email stephan@g2design.co.za',
 			'git add -A *',
 			'git commit -m "'.$message.'"',
-			'git push'
+			'git push',
+			'git commit --amend --reset-author'
 		];
 
 		$out = run_commands($commands);
