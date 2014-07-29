0b7480e9fe1377b7532a22bc047405e4c44e3745
Stephan Wessels <stephan@g2design.co.za>
2014-07-29 18:25:53 +0200

    Updated table generator class to support custom queries Also add custom query paginate function to db class

--- library/Mvc/Db.php
+++ library/Mvc/Db.php
@@ -1,7 +1,11 @@
 <?php
-include_once SYSTEM_DIR.'/includes/rb.phar';
-class Mvc_Db extends R{
-	private static $dsn,$username,$password,$frozen,$connected,$page_count,$current_page;
+
+include_once SYSTEM_DIR . '/includes/rb.phar';
+
+class Mvc_Db extends R {
+
+	private static $dsn, $username, $password, $frozen, $connected, $page_count, $current_page;
+
 	public static function setup_db($dsn = NULL, $username = NULL, $password = NULL, $frozen = FALSE) {
 		self::$dsn = $dsn;
 		self::$username = $username;
@@ -10,7 +14,7 @@ class Mvc_Db extends R{
 	}
 
 	public static function setup() {
-		if(self::$connected == false){
+		if (self::$connected == false) {
 			parent::setup(self::$dsn, self::$username, self::$password, self::$frozen);
 			self::$connected = false;
 		}
@@ -27,24 +31,36 @@ class Mvc_Db extends R{
 	public static function paginate_findAll($type, $limit, $sql = NULL, $bindings = array()) {
 
 		//Get the current Page
-		if(isset($_GET['p'])){
+		if (isset($_GET['p'])) {
 			$page = $_GET['p'];
-		} else $page = 1;
+		} else
+			$page = 1;
 		self::$current_page = $page;
 		self::$page_count = ceil(R::count($type, $sql, $bindings) / $limit);
-		return self::findAll($type, $sql.' LIMIT '.(($page-1)* $limit ).', '.$limit, $bindings);
+		return self::findAll($type, $sql . ' LIMIT ' . (($page - 1) * $limit ) . ', ' . $limit, $bindings);
+	}
+
+	public static function paginate_query($sql, $limit) {
+		if (isset($_GET['p'])) {
+			$page = $_GET['p'];
+		} else
+			$page = 1;
+		self::$current_page = $page;
+		$data = self::getAll($sql . ' LIMIT ' . (($page - 1) * $limit ) . ', ' . $limit, $bindings);
+		self::$page_count = ceil(count(self::getAll($sql)) / $limit);
+		return $data;
 	}
 
 	/**
 	 * Returns the last paginate_findAll page count
 	 * @return type
 	 */
-	public static function get_last_total_pages(){
+	public static function get_last_total_pages() {
 		return self::$page_count;
 	}
 
-	public static function get_current_page(){
+	public static function get_current_page() {
 		return self::$current_page;
 	}
-}
 
+}
