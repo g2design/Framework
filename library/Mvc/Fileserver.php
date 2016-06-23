ef81ccf8e968b2820a76ddca27ff92304c8a59f6
StephanAtG2 <stephan@g2design.co.za>
2016-06-23 09:46:17 +0200

    Css Function

--- library/Mvc/Fileserver.php
+++ library/Mvc/Fileserver.php
@@ -3,49 +3,47 @@
 class Mvc_Fileserver extends Mvc_Singleton {
 
 	var $blocked = [], $locations = ['cache'];
-	
+
 	public function add_location($location) {
 		$this->locations[] = $location;
 	}
-	
-	
-	
+
 	public function serve($file) {
 		set_time_limit(0);
 		//Process the query
 		$extension = Mvc_Functions::get_extension($file);
 
 		if (in_array($extension, $this->blocked)) {
-			echo "GET HERE";exit;
+			echo "GET HERE";
+			exit;
 			$this->_404();
 		} else {
 			$this->serve_file($this->get_file($file));
 		}
 	}
-	
+
 	public function exists($file) {
 		$extension = Mvc_Functions::get_extension($file);
 		if (in_array($extension, $this->blocked)) {
 			return false;
 		}
-		
-		if($this->get_file($file) !== false) {
+
+		if ($this->get_file($file) !== false) {
 			return true;
 		} else {
 			return false;
 		}
 	}
-	
+
 	protected function get_file($file) {
 		$file_n = false;
-		foreach($this->locations as $location) {
-			if(file_exists($location.'/'.$file)) {
-				$file_n = $location.'/'.$file;
+		foreach ($this->locations as $location) {
+			if (file_exists($location . '/' . $file)) {
+				$file_n = $location . '/' . $file;
 				break;
 			}
 		}
 		return $file_n;
-		
 	}
 
 	/**
@@ -60,7 +58,7 @@ class Mvc_Fileserver extends Mvc_Singleton {
 			$extension = Mvc_Functions::get_extension($file);
 			$func = "{$extension}_mime";
 			if (method_exists($this, $func)) {
-				$this->$func($file_location);
+				$this->$func($file);
 			} else {
 				$mimepath = '/usr/share/magic'; // may differ depending on your machine
 				// try /usr/share/file/magic if it doesn't work
@@ -105,6 +103,16 @@ class Mvc_Fileserver extends Mvc_Singleton {
 		echo "
 			<h1>404 page does not exist</h1>
 		";
+	}
+
+	function css_mime($file) {
+		header("Content-Type: text/css");
+		header("X-Content-Type-Options: nosniff");
+		header("Access-Control-Allow-Origin:*");
+		header('Cache-Control:public, max-age=30672000');
+
+		echo file_get_contents($file);
+		die();
 	}
 
 }
